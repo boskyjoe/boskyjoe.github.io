7198fd5e9dece603aa157816c760702e78211c68
boskyjoe <boskyjoe@gmail.com>
2025-09-02 12:07:36 -0700

    Create state.js

---
+++ TrinityCart/js/state.js
@@ -0,0 +1,5 @@
+// This object is the "single source of truth" for our application's state.
+export const appState = {
+    currentUser: null, // Holds user info { name, email, role } when logged in.
+    currentSeason: null, // Will hold the active season object.
+};
