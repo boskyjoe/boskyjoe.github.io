3df4afa01797c7a652fc634278c7ca127e6b00db
boskyjoe <boskyjoe@gmail.com>
2025-09-02 18:00:09 -0700

    Update config.js

--- TrinityCart/js/config.js
+++ TrinityCart/js/config.js
@@ -1,6 +1,7 @@
 // This will hold the URL of our deployed Google Apps Script.
 // We will fill this in after we create the backend.
-export const API_URL = 'https://script.google.com/macros/s/AKfycbxeZvciS8Dc-FE_wZcXpcXswjSvmgk1Z9-2rI5n9T2mtqZznEG7wrg1gceAmUxuSyQ0/exec'; 
+export const API_URL = 'https://script.google.com/macros/s/AKfycbx6Il42-EAqFbiY8EuaZ6U_hhJbRecl6cSiFz6SPkPy4kEZsAd2DZGLQ0OHF7konnKn/exec'; 
+
 
 
 
