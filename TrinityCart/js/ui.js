95ba72e52121239fce0853ee6be44e3012bf5501
boskyjoe <boskyjoe@gmail.com>
2025-09-24 13:58:03 -0700

    Update ui.js

--- TrinityCart/js/ui.js
+++ TrinityCart/js/ui.js
@@ -1650,8 +1650,8 @@ function syncAvailableProductsGrid() {
     catalogueItemsGridApi.forEachNode(node => currentItems.push(node.data));
     console.log(`Found ${currentItems.length} items in the right-side grid.`);
 
-    currentCatalogueItemProductIds = new Set(currentItems.map(item => item.productId));
-    console.log("Updated Set of current product IDs:", currentCatalogueItemProductIds);
+    currentCatalogueItemIds = new Set(currentItems.map(item => item.productId));
+    console.log("Updated Set of current product IDs:", currentCatalogueItemIds);
 
     console.log("Calling refreshCells() on the left-side grid to update buttons...");
     availableProductsGridApi.refreshCells({
@@ -1694,7 +1694,7 @@ const availableProductsGridOptions = {
                 const productId = params.data.id;
                 const addIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5z" /></svg>`;
                 
-                const isDuplicate = currentCatalogueItemProductIds.has(productId);
+                const isDuplicate = currentCatalogueItemIds.has(productId);
                 
                 console.log(`Rendering 'Add' button for product ${productId}. Is it a duplicate? ${isDuplicate}`);
 
