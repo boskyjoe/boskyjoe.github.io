d1b26097a9d4eed40647a91b9f1bc3dc2d04fa2c
boskyjoe <boskyjoe@gmail.com>
2025-10-11 14:56:37 -0700

    Update ui.js

--- TrinityCart/js/ui.js
+++ TrinityCart/js/ui.js
@@ -3924,7 +3924,7 @@ const salePaymentHistoryGridOptions = {
             cellRenderer: params => {
                 // Only show the Void button for verified, non-voided payments
                 if (params.data && params.data.status === 'Verified' && appState.currentUser.role === 'admin') {
-                    const voidIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M15.312 1.312a2.25 2.25 0 0 0-3.182 0l-10 10a2.25 2.25 0 0 0 0 3.182l10 10a2.25 2.25 0 0 0 3.182-3.182L6.818 11.25h10.432a.75.75 0 0 0 0-1.5H6.818l8.494-8.494a2.25 2.25 0 0 0 0-3.182Z" clip-rule="evenodd" /></svg>`;
+                    const voidIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM8.28 7.22a.75.75 0 0 0-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L10 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L11.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L10 8.94 8.28 7.22Z" clip-rule="evenodd" /></svg>`;
                     return `<button class="action-btn-icon action-btn-delete action-btn-void-sale-payment" data-id="${params.data.id}" title="Void Payment">${voidIcon}</button>`;
                 }
                 return '';
