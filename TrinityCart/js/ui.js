4aecadf948ee8c20b95b9d5df869fc8cfe4023d5
boskyjoe <boskyjoe@gmail.com>
2025-10-11 15:03:04 -0700

    Update ui.js

--- TrinityCart/js/ui.js
+++ TrinityCart/js/ui.js
@@ -3925,7 +3925,7 @@ const salePaymentHistoryGridOptions = {
                 // Only show the Void button for verified, non-voided payments
                 if (params.data && params.data.status === 'Verified' && appState.currentUser.role === 'admin') {
                     const voidIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM8.28 7.22a.75.75 0 0 0-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L10 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L11.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L10 8.94 8.28 7.22Z" clip-rule="evenodd" /></svg>`;
-                    return `<button class="action-btn-icon action-btn-delete action-btn-void-sale-payment" data-id="${params.data.id}" title="Void Payment">${voidIcon}</button>`;
+                    return `<button class="action-btn-icon action-btn-delete text-red-500 action-btn-void-sale-payment" data-id="${params.data.id}" title="Void Payment">${voidIcon}</button>`;
                 }
                 return '';
             }
