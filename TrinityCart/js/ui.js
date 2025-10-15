8331656000471d589a47d8014ccdc30f42bb84eb
boskyjoe <boskyjoe@gmail.com>
2025-10-14 23:51:30 -0700

    Update ui.js

--- TrinityCart/js/ui.js
+++ TrinityCart/js/ui.js
@@ -1674,7 +1674,7 @@ const purchasePaymentsGridOptions = {
         },
         {
             headerName: "Supplier",
-            width: 200,
+            width: 300,
             pinned: 'left', // This also freezes the column
             // Use a valueGetter to look up the supplier name from masterData
             valueGetter: params => {
@@ -1690,10 +1690,11 @@ const purchasePaymentsGridOptions = {
             flex: 1,
             valueFormatter: p => p.value ? formatCurrency(p.value) : ''
         },
-        { field: "paymentMode", headerName: "Mode", flex: 1 },
-        { field: "transactionRef", headerName: "Reference #", flex: 2 },
+        { field: "paymentMode", headerName: "Mode", width: 300,flex: 1 },
+        { field: "notes", headerName: "Notes", width: 300, flex: 1 },
+        { field: "transactionRef", headerName: "Reference #", width: 300, flex: 2 },
         {
-            headerName: "Actions", width: 80, cellClass: 'flex items-center justify-center',
+            headerName: "Actions", width: 100, cellClass: 'flex items-center justify-center',
             cellRenderer: params => {
                 // Re-use the same trash can icon from other grids for consistency
                 const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.58.22-2.365.468a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5z" clip-rule="evenodd" /></svg>`;
