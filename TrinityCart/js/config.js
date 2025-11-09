5ae1bf339eb5b302c26363ca6d48a49c54c7d65b
boskyjoe <boskyjoe@gmail.com>
2025-11-08 16:30:01 -0800

    Update config.js

--- TrinityCart/js/config.js
+++ TrinityCart/js/config.js
@@ -281,7 +281,7 @@ export const navConfig = [
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
         </svg>`,
         viewId: "dashboard-view",
-        roles: ["admin", "inventory_manager", "sales_staff", "finance", "team_lead"]
+        roles: ["admin", "inventory_manager", "sales_staff", "finance", "team_lead","guest"]
     },
     
     // 2. Product Management - Outline
@@ -326,7 +326,7 @@ export const navConfig = [
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>`,
         viewId: "sales-view",
-        roles: ["admin", "sales_staff", "team_lead"]
+        roles: ["admin", "sales_staff", "team_lead","guest"]
     },
     
     // 6. Consignment - Outline
