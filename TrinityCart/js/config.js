47cf4360a1b7b40072f6724df318fc762b9e471c
boskyjoe <boskyjoe@gmail.com>
2025-11-15 00:27:01 -0800

    Update config.js

--- TrinityCart/js/config.js
+++ TrinityCart/js/config.js
@@ -218,7 +218,7 @@ export const navConfig = [
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>`,
         viewId: "sales-view",
-        roles: ["admin", "sales_staff", "team_lead","guest"]
+        roles: ["admin", "sales_staff", "team_lead","guest","inventory_manager"]
     },
     
     // 6. Consignment - Outline
@@ -229,7 +229,7 @@ export const navConfig = [
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
         </svg>`,
         viewId: "consignment-view",
-        roles: ["admin", "team_lead", "sales_staff", "finance", "guest"]
+        roles: ["admin", "team_lead", "sales_staff", "finance", "guest","inventory_manager"]
     },
     
     // 7. Supplier - Outline
