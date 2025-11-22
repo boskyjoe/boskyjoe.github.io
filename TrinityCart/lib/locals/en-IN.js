cc8a327f3a8ab9b53980c575d71510be6ad97f1e
boskyjoe <boskyjoe@gmail.com>
2025-11-22 08:09:18 -0800

    Create en-IN.js

---
+++ TrinityCart/lib/locals/en-IN.js
@@ -0,0 +1,8 @@
+/**
+ * Minified by jsDelivr using Terser v5.39.0.
+ * Original file: /npm/to-words@4.8.0/dist/locales/en-IN.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});class Locale{constructor(){this.config={currency:{name:"Rupee",plural:"Rupees",singular:"Rupee",symbol:"₹",fractionalUnit:{name:"Paisa",plural:"Paise",singular:"Paisa",symbol:""}},texts:{and:"And",minus:"Minus",only:"Only",point:"Point"},numberWordsMapping:[{number:1e7,value:"Crore"},{number:1e5,value:"Lakh"},{number:1e3,value:"Thousand"},{number:100,value:"Hundred"},{number:90,value:"Ninety"},{number:80,value:"Eighty"},{number:70,value:"Seventy"},{number:60,value:"Sixty"},{number:50,value:"Fifty"},{number:40,value:"Forty"},{number:30,value:"Thirty"},{number:20,value:"Twenty"},{number:19,value:"Nineteen"},{number:18,value:"Eighteen"},{number:17,value:"Seventeen"},{number:16,value:"Sixteen"},{number:15,value:"Fifteen"},{number:14,value:"Fourteen"},{number:13,value:"Thirteen"},{number:12,value:"Twelve"},{number:11,value:"Eleven"},{number:10,value:"Ten"},{number:9,value:"Nine"},{number:8,value:"Eight"},{number:7,value:"Seven"},{number:6,value:"Six"},{number:5,value:"Five"},{number:4,value:"Four"},{number:3,value:"Three"},{number:2,value:"Two"},{number:1,value:"One"},{number:0,value:"Zero"}],exactWordsMapping:[{number:100,value:"One Hundred"}]}}}exports.default=Locale;
+//# sourceMappingURL=/sm/d81a8e6f820dd49cf99b41aeec53560cbd156764e70d5dc37280eb1fbf58168d.map
