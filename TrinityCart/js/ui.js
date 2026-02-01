b953d1bc84234ed5acd7071fc8376023cc52e6ae
boskyjoe <boskyjoe@gmail.com>
2026-02-01 01:08:57 -0800

    Update ui.js - removed the action buttons for the leads grid

--- TrinityCart/js/ui.js
+++ TrinityCart/js/ui.js
@@ -1667,21 +1667,7 @@ const leadsGridOptions = {
             cellEditor: 'agDateCellEditor',
             valueFormatter: p => p.value ? (p.value.toDate ? p.value.toDate() : new Date(p.value)).toLocaleDateString() : ''
         },
-        {
-            headerName: "Actions",
-            width: 120,
-            cellClass: 'flex items-center justify-center space-x-2',
-            cellRenderer: params => {
-                const leadId = params.data.id;
-                const editIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" /><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" /></svg>`;
-                const convertIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" /></svg>`;
-                return `
-                    <button title="Edit Lead" class="text-blue-600 hover:text-blue-800 edit-lead-btn" data-id="${leadId}">${editIcon}</button>
-                    <button title="Convert to Sale" class="text-green-600 hover:text-green-800 convert-lead-btn" data-id="${leadId}">${convertIcon}</button>
-                `;
-            },
-            editable: false, sortable: false, filter: false
-        }
+        
     ],
     defaultColDef: {
         sortable: true, filter: true, resizable: true, wrapText: true, autoHeight: true,
