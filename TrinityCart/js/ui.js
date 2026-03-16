fdbd663423263544d2f8b3cd591315b2788a3b47
boskyjoe <boskyjoe@gmail.com>
2026-03-16 01:12:37 -0700

    Update ui.js removed the strickthrught product name

--- TrinityCart/js/ui.js
+++ TrinityCart/js/ui.js
@@ -17436,7 +17436,7 @@ export function showConsignmentModalV2(orderData = null) {
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                                 </svg>
-                                <span class="line-through text-gray-400">${params.value}</span>
+                                <span class="text-gray-500 font-medium">${params.value}</span>
                             </div>
                         `;
                     } else {
